03aece16b7acd0a999fe3042c27bcc75b84db355
caselongo <caselongo@gmail.com>
2019-04-05 17:50:38 +0200

    x

---
+++ app/constants.php
@@ -0,0 +1,21 @@
+<?php
+namespace App;
+
+class Constants {
+	
+   const StravaClientId = 8752;
+   const StravaClientSecret = '149ff5792ba900da9a5beb578a03051dbb0cf3ba';
+   
+   const StravaActivitiesUrl = 'https://www.strava.com/activities/';
+   
+   //const ImagesPath = '/images';
+   const ImagesPath = 'https://www.cyclingcols.com/images';
+   
+   //const ProfilesPath = '/profiles';
+   const ProfilesPath = 'https://www.cyclingcols.com/profiles';
+   
+   //const ToursPath = '/tours';
+   const ToursPath = 'https://www.cyclingcols.com/tours';
+}
+
+?>
