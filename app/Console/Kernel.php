17c77a2d3d189652c7bd3bf82ff1a074bdaafe34
caselongo <caselongo@gmail.com>
2019-04-09 18:19:35 +0200

    scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+		$schedule->command('queue:work --queue=athlete --sleep=5 --tries=3 --daemon &')->everyMinute();
     }
 
     /**
