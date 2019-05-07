b06679ac09ffe8ef191a45c387817d599b1fddf2
caselongo <caselongo@gmail.com>
2019-05-07 11:07:53 +0200

    x

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -16,7 +16,7 @@
 ?>
 
 <header>
-	<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+	<nav class="navbar navbar-expand-sm navbar-dark bg-dark pb-0">
 		<a class="navbar-brand" href="/"><img id="logo_img" src="/images/logo.png" /></a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
